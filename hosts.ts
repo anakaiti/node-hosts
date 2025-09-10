98bad08e2c0e2b5fe0b1df09ec32a308a8b71811
Yahya <yahya@anakaiti.net>
2025-09-10 19:41:22 +0800

    Initial

---
+++ hosts.ts
@@ -0,0 +1,16 @@
+/**
+ * A module that contains the string "hosts".
+ * 
+ * @example Logging the string "hosts":
+ * ```ts
+ * import hosts from "@etc/hosts";
+ * console.log(hosts);
+ * ```
+ * 
+ * @module
+ */
+
+
+const hosts = "hosts";
+
+export default hosts;
